9d71cbf2654c4f6b54823811146f5da5165a2d33
TotoroKingdom <634581304@qq.com>
2026-07-10 00:07:26 +0800

    feat: chatbot相关内容

---
+++ paiguangguang/frontend/app/chat-bot/page.tsx
@@ -0,0 +1,9 @@
+import { ChatBotPanel } from "@/features/chat-bot/chat-bot-panel";
+
+export default function ChatBotPage() {
+  return (
+    <div className="mx-auto w-full max-w-6xl">
+      <ChatBotPanel />
+    </div>
+  );
+}
